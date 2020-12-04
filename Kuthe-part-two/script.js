2af32bf3239a3969132024438bb5bcab9ea90df8
pskuthe30 <34254065+pskuthe30@users.noreply.github.com>
2020-12-04 08:44:36 -0500

    Add files via upload
    Made changes in index and script file. indentation and checking value in caps

--- Kuthe-part-two/script.js
+++ Kuthe-part-two/script.js
@@ -1,19 +1,17 @@
-
-    
+   
     var rainfall=0;
-    while(rainfall==0 || rainfall=== null){
+    while(rainfall==0){
       rainfall = prompt ("How many inches of Rain fell?");
     } 
  
- 
- for (let i=1 ; i <= rainfall; i++) {
+ for (var i=1 ; i <= rainfall; i++) {
     document.write( "*" );
     }
     
     var answer= null;
     answer= prompt("Did you use fertilizer?")
 
-if (answer=="no")
+if (answer=="no" || answer=="NO")
 {
   if (rainfall >=20){
        document.write("<br />"+"The yield should be 45 bushels per acre");}
@@ -24,8 +22,8 @@ if (answer=="no")
 else {
     var brand = prompt("<br />"+" Did you use premium or regular fertilizer?");
    
-    if (brand=="premium")
-    {
+    if (brand=="premium"|| brand =="PREMIUM")
+       {
       if (rainfall > 20) 
           document.write("<br />"+"The yield should be 51.45 bushnels per acre");
           else if (rainfall <= 20)
